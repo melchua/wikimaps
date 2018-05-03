7d5068f7c4346492dedef5bace84b7ee91783587
mel <mchuam@gmail.com>
2018-05-03 06:03:27 +0000

    Added javascript to get maps loading initially. Edited some routes, but we will likely need to update more

---
+++ routes/maps.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (knex) => {
+
+  router.get("/", (req, res) => {
+    res.render("index");
+  });
+
+  return router;
+}
--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,14 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  router.get("/", (req, res) => {
+  router.get("/users", (req, res) => {
     knex
       .select("*")
       .from("users")
       .then((results) => {
         res.json(results);
     });
+
   });
 
   return router;

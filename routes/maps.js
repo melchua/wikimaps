a4ea60245d3df29264243d10a80fef91f4017247
mel <mchuam@gmail.com>
2018-05-05 21:59:16 +0000

    save marker and render marker capability added

--- routes/maps.js
+++ routes/maps.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router  = express.Router();
 
+
 module.exports = (mapActions) => {
 
   router.get("/", (req, res) => {
@@ -13,5 +14,34 @@ module.exports = (mapActions) => {
       });
   });
 
+  router.get("/:id", (req, res) => {
+    mapActions.getPlacesByMapId(req.params.id, function(result){
+      res.json({data: result});
+    });
+   });
+
+    // mapActions.getMaps()
+    //   .then((maps) => {
+    //     res.json(maps);
+
+    //   });
+
+
+
+  router.post("/places", (req, res) => {
+    console.log('maps post success');
+
+    const newPlace = {
+      name: req.body.name,
+      description: req.body.description,
+      img: req.body.img,
+      lat: req.body.lat,
+      lng: req.body.lng
+    };
+
+    mapActions.createPlace(newPlace);
+      // .then(());
+
+  });
   return router;
 };
